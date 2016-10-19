97f38f1a668b7ec82f56ab92bd5618a942efc8a3
ideayuye <576135266@qq.com>
2016-10-19 23:09:55 +0800

    input func

--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
 
 var grapher = null;
 function myInit() {
-    grapher = new Grapher();
     var canvas = document.getElementById('mainCanvas');
     var w = window.innerWidth;
     var h = window.innerHeight;
     canvas.setAttribute('width',w);
     canvas.setAttribute('height',h);
-    
+    grapher = new Grapher();
+    var $input = $('#input-panel input');
+    $input.change(function(){
+        var formula = $(this).val().trim();
+        var x = 1.1,y=0,isError = 0;
+        try{
+            with( Math ) { eval("y = " + formula); }
+        }catch(e){
+            isError = 1;
+        }
+        if(!isError){
+            grapher.formula = formula;
+            grapher.draw();
+        }
+    });
 }
 myInit();
-// grapher.drawGraph("sin(x)", '#ffc040');
 grapher.formula = "sin(x)";
 grapher.draw();
 
