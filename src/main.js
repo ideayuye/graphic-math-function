3c94ab3e8ab09590ae19c4c577c797ed2e54ceb3
ideayuye <576135266@qq.com>
2016-10-21 18:10:33 +0800

    hammer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 var grapher = null;
+var canvas = document.getElementById('mainCanvas');
 function myInit() {
-    var canvas = document.getElementById('mainCanvas');
     var w = window.innerWidth;
     var h = window.innerHeight;
     canvas.setAttribute('width',w);
@@ -25,4 +25,36 @@ function myInit() {
 myInit();
 grapher.formula = "sin(x)";
 grapher.draw();
+
+
+//hammer test
+var $input = $('#input-panel input');
+var ci = 0,pi= 0;
+var hammer = new Hammer(canvas);
+    hammer.get('pinch').set({enable:true});
+
+/*hammer.on('pan',function(ev){
+    console.log(ev);
+    $input.val('pan'+ ci++);
+});*/
+
+/*hammer.on('pinch',function(){
+    console.log('pinch');
+    $input.val('pinch'+ pi++);
+});*/
+
+/*hammer.on('pinchstart',function(ev){
+    console.log('pinchstart');
+    $input.val('pinchstart'+ pi++);
+});*/
+
+var px = 0;
+hammer.on('pinchin',function(ev){
+    console.log('pinchin');
+    var scale = 1;
+    scale = ev.originalEvent.gesture.scale;
+    $input.val('pinchin'+ px++ +":"+ scale);
+});
+
+
 
