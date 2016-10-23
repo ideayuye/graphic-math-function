5b06fe94b0834523b25272352b161b6acb3d250d
ideayuye <576135266@qq.com>
2016-10-23 23:02:44 +0800

    help

--- src/main.js
+++ src/main.js
@@ -4,30 +4,40 @@ var canvas = document.getElementById('mainCanvas');
 function myInit() {
     var w = window.innerWidth;
     var h = window.innerHeight;
-    canvas.setAttribute('width',w);
-    canvas.setAttribute('height',h);
+    canvas.setAttribute('width', w);
+    canvas.setAttribute('height', h);
     grapher = new Grapher();
 
+    var helpComponet = Vue.extend({
+        template: "<div class='help' v-bind:class=\"{'hide':ishide}\">help conponent {{ishide}}</div>",
+        props:['ishide']
+    });
+    Vue.component('help', helpComponet);
+
     new Vue({
-        el:'#input-panel',
-        data:{
-            formula:''
+        el: '#input-panel',
+        data: {
+            formula: '',
+            ishide : true
         },
-        methods:{
-            changeFormula:function(){
-                 var formula = this.formula;
-                if(!grapher.notOnBlackList(formula))
+        methods: {
+            changeFormula: function () {
+                var formula = this.formula;
+                if (!grapher.notOnBlackList(formula))
                     return;
-                var x = 1.1,y=0,isError = 0;
-                try{
-                    with( Math ) { eval("y = " + formula); }
-                }catch(e){
+                var x = 1.1, y = 0, isError = 0;
+                try {
+                    with (Math) { eval("y = " + formula); }
+                } catch (e) {
                     isError = 1;
                 }
-                if(!isError){
+                if (!isError) {
                     grapher.formula = formula;
                     grapher.draw();
                 }
+            },
+            helpClick :function(){
+                this.ishide = !this.ishide;
             }
         }
     });
@@ -38,22 +48,17 @@ grapher.draw();
 
 //hammer test
 var hammer = new Hammer(canvas);
-    hammer.get('pinch').set({enable:true});
+hammer.get('pinch').set({ enable: true });
 
-hammer.on('pan',function(ev){
+hammer.on('pan', function (ev) {
     grapher.mPan(ev);
 });
 
-hammer.on('pinchstart',function(ev){
-    grapher.mScaleStart();RTCStatsProvider
+hammer.on('pinchstart', function (ev) {
+    grapher.mScaleStart();
 });
 
-hammer.on('pinch',function(ev){
+hammer.on('pinch', function (ev) {
     grapher.mScale(ev);
 });
-
-
-
-
-
 
